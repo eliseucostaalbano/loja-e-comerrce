8e8504a82d5250685d6e1d977e1f05ecc1b40166
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-07-15 16:08:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./Componetes/Login/Login"
+function App() {
+
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Login />
+
+    </div>
+  )
+}
+
+export default App
