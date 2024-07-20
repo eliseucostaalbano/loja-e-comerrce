1df28eb3da7b8a8fab681c5d3565130a99a0df2f
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-07-20 23:56:02 +0000

    simplicando o filtro

--- src/components/ecommerce/Filtro.jsx
+++ src/components/ecommerce/Filtro.jsx
@@ -1,118 +1,58 @@
-import React, { useState } from 'react';
-
-const produtos = [
-  { id: 1, marca: 'Adidas', categoria: 'Esporte', genero: 'Masculino', estado: 'Novo' },
-  { id: 2, marca: 'Calecianga', categoria: 'Casual', genero: 'Feminino', estado: 'Usado' },
-  { id: 3, marca: 'K-Swiss', categoria: 'Utilitário', genero: 'Unissex' },
-  { id: 4, marca: 'Nike', categoria: 'Corrida'},
-  { id: 5, marca: 'Puma'}
-];
-
-
-const Filtro = () => {
-  const [filtros, setFiltros] = useState({
-    marcas: [],
-    categorias: [],
-    generos: [],
-    estado: ''
-  });
-
-  const handleCheckboxChange = (e, category) => {
-    const { name, checked } = e.target;
-    setFiltros((prevState) => {
-      const categoryFilters = prevState[category];
-      if (checked) {
-        return {
-          ...prevState,
-          [category]: [...categoryFilters, name],
-        };
-      } else {
-        return {
-          ...prevState,
-          [category]: categoryFilters.filter((item) => item !== name),
-        };
-      }
-    });
-  };
-
-  const handleRadioChange = (e) => {
-    const { name } = e.target;
-    setFiltros((prevState) => ({
-      ...prevState,
-      estado: name,
-    }));
-  };
-
-
-
+export default function Filtro() {
   return (
-    <div className="flex mb-[390px] mr-4">
+    <div className="flex mb-[400px] mr-4">
       <div className="bg-white p-4 w-[250px]">
         <h1 className="mb-3 text-xl font-bold">Filtrar por</h1>
-        <div className="border-b mb-4"></div>
-
-        <div>
-          <p className="font-bold mb-2">Marcas</p>
-          {['Adidas', 'Calecianga', 'K-Swiss', 'Nike', 'Puma'].map((marca) => (
-            <div key={marca} className="mb-2">
-              <input
-                type="checkbox"
-                name={marca}
-                className="form-checkbox text-[#C92071]"
-                onChange={(e) => handleCheckboxChange(e, 'marcas')}
-              />
-              <label htmlFor={marca} className="ml-2">{marca}</label>
-            </div>
-          ))}
-        </div>
-
+        <div className="border-b mb-4 "></div>
         <div className="mt-4">
-          <p className="font-bold mb-2 text-black">Categoria</p>
-          {['Esporte', 'Casual', 'Utilitário', 'Corrida'].map((categoria) => (
-            <div key={categoria} className="mb-2">
-              <input
-                type="checkbox"
-                name={categoria}
-                className="form-checkbox text-[#C92071]"
-                onChange={(e) => handleCheckboxChange(e, 'categorias')}
-              />
-              <label htmlFor={categoria} className="ml-2">{categoria}</label>
-            </div>
-          ))}
+          <p className="font-bold mb-2 text-xl">Marcas</p>
+          <div className="mb-2">
+          <input type="checkbox" name="Adidas" value="Adidas" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Adidas" className="ml-2 text-xl">Adiadas</label><br></br>
+            <input type="checkbox" name="Calecianga" value="Calecianga" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Calecianga" className="ml-2 text-xl">Calencianga</label><br></br>
+            <input type="checkbox" name="K-Swiss" value="K-Swiss" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="K-Swiss" className="ml-2 text-xl">K-Swiss</label><br></br>
+            <input type="checkbox" name="Nike" value="Nike" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Nike" className="ml-2 text-xl">Nike</label><br></br>
+            <input type="checkbox" name="Puma" value="Puma" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Puma" className="ml-2 text-xl">Puma</label>
+          </div>
         </div>
-
         <div className="mt-4">
-          <p className="font-bold mb-2">Gênero</p>
-          {['Masculino', 'Feminino', 'Unissex'].map((genero) => (
-            <div key={genero} className="mb-2">
-              <input
-                type="checkbox"
-                name={genero}
-                className="form-checkbox text-[#C92071]"
-                onChange={(e) => handleCheckboxChange(e, 'generos')}
-              />
-              <label htmlFor={genero} className="ml-2">{genero}</label>
-            </div>
-          ))}
+          <p className="font-bold mb-2 text-xl">Categoria</p>
+          <div className="mb-2">
+          <input type="checkbox" name="Esporte" value="Esporte" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Adidas" className="ml-2 text-xl">Esporte</label><br></br>
+            <input type="checkbox" name="Casual" value="Casual" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Casual" className="ml-2 text-xl">Casual</label><br></br>
+            <input type="checkbox" name="Utilitario" value="Utilitario" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Utilitario" className="ml-2 text-xl">Utilitario</label><br></br>
+            <input type="checkbox" name="Corrida" value="Corrida" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Corrida" className="ml-2 text-xl">Corrida</label>
+          </div>
         </div>
-
         <div className="mt-4">
-          <p className="font-bold mb-2">Estado</p>
-          {['Novo', 'Usado'].map((estado) => (
-            <div key={estado} className="mb-2">
-              <input
-                type="radio"
-                name={estado}
-                className="form-radio text-[#C92071]"
-                onChange={handleRadioChange}
-              />
-              <label htmlFor={estado} className="ml-2">{estado}</label>
-            </div>
-          ))}
+          <p className="font-bold mb-2 text-xl">Gênero</p>
+          <div className="mb-2">
+          <input type="checkbox" name="Masculino" value="Masculino" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Masculino" className="ml-2 text-xl">Masculino</label><br></br>
+            <input type="checkbox" name="Feminino" value="Feminino" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Feminino" className="ml-2 text-xl">Feminino</label><br></br>
+            <input type="checkbox" name="Unissex" value="Unissex" className="form-checkbox text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Unissex" className="ml-2 text-xl">Unissex</label>
+          </div>
+        </div>
+        <div className="mt-4">
+          <p className="font-bold mb-2 text-xl">Marcas</p>
+          <div className="mb-2">
+          <input type="radio" name="Novo" value="Novo" className="form-radio text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Novo" className="ml-2 text-xl">Novo</label><br></br>
+            <input type="radio" name="Usado" value="Usado" className="form-radio text-[#C92071] checked:bg-[#C92071] checked:border-transparent" />
+            <label htmlFor="Usado" className="ml-2 text-xl">Usado</label>
+          </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default Filtro;
+}
